7177f9b3d2aec09f471e5b0236da24c3a5549138
AnaMakatsaria <anushki1@gmail.com>
2024-04-27 01:16:11 -0400

    project4 done

---
+++ src/main/java/org/example/Task10.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+/*Find the First Non-Repeating Character in a String: Given a string, find and
+return the first non-repeating character. For example, in the string
+"abracadabra", the first non-repeating character is 'c'.*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Set<Integer>numbers=new HashSet<>();
+        numbers.add(25);
+        numbers.add(25);
+        numbers.add(33);
+        numbers.add(22);
+        numbers.add(25);
+        System.out.println(numbers);
+    }
+
+}
